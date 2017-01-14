e1e7957724b4537a2ab795cfe13c274192829356
Kalab J Oster <Kalab_J_Oster@localhost>
2017-01-14 15:45:04 -0800

    Write outline and init and some checking for the Narrative object. ...Write test data in the test class.

---
+++ _pytest/test_narrator.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+test_greet = {
+  "intent" : "greet",
+  "state" : "initial",
+  "response" : [
+      "initial": "Hi",
+      "continued" : "Hi?",
+      "annoyed" : "HI!",
+      "bored": "...Hi..."
+  ]
+
+}
